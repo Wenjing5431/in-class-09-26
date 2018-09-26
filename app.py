8cd11205d48e12d67f8450b04d4086830565482b
Max Harper <maxharp3r@gmail.com>
2018-09-25 22:27:42 -0500

    add base template and sql insert

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, render_template, url_for
+from flask import Flask, redirect, render_template, request, url_for
 import psycopg2
 
 import db
@@ -12,17 +12,25 @@ def initialize():
     db.setup()
 
 @app.route('/')
-def hello_world():
+def home():
     link = url_for("people")
-    return f'Hello World! <a href="{link}">people</a>'
+    # return f'Hello World! <a href="{link}">people</a>'
+    return render_template("home.html", link=link)
 
-@app.route('/people')
+@app.route('/people', methods=['GET', 'POST'])
 def people():
-    with db.get_db_cursor() as cur:
-        cur.execute("SELECT * FROM person;")
-        names = [record["name"] for record in cur]
+    if request.method == 'POST':
+        name = request.form['name']
+        app.logger.info(f"got a name: {name}")
+        with db.get_db_cursor(commit=True) as cur:
+            cur.execute("insert into person (name) values (%s)", (name,))
+        return redirect(url_for("people"))
+    else:
+        with db.get_db_cursor() as cur:
+            cur.execute("SELECT * FROM person;")
+            names = [record["name"] for record in cur]
 
-    return render_template("people.html", names=names)
+        return render_template("people.html", names=names)
 
 if __name__ == '__main__':
     pass
