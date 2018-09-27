7b25c3adc74013a2f9894b16610b8d478a9ea8ca
Hao Fei Cheng <cheng635@wildcat-umh.cs.umn.edu>
2018-09-27 15:59:07 -0500

    in class exercise

--- app.py
+++ app.py
@@ -18,13 +18,12 @@ def home():
     return render_template("home.html", link=link)
 
 
-@app.route('/people', methods=['GET', 'POST'])
+@@app.route('/people', methods=['GET', 'POST'])
 def people():
     if request.method == 'POST':
         name = request.form['name']
-        app.logger.info(f"got a name: {name}")
         with db.get_db_cursor(commit=True) as cur:
-            cur.execute("insert into person (name) values (%s)", (name,))
+            cur.execute("INSERT INTO person (name) VALUES (%s)", (name,))
         return redirect(url_for("people"))
     else:
         with db.get_db_cursor() as cur:
@@ -32,6 +31,21 @@ def people():
             names = [record["name"] for record in cur]
 
         return render_template("people.html", names=names)
+
+@app.route('/people/<username>', methods=['GET'])
+def userprofile(username):
+    with db.get_db_cursor() as cur:
+        cur.execute("SELECT * FROM person WHERE name = %s;", (username,))
+        user = cur.fetchall()
+    return render_template("profile.html", user = user[0])
+
+@app.route('/editprofile', methods=['POST'])
+def editprofile():
+    userid = request.form['userid']
+    name = request.form['name']
+    with db.get_db_cursor(commit=True) as cur:
+        cur.execute("UPDATE person SET name = %s WHERE person_id = %s", (name, userid))
+    return redirect(url_for("people"))
 
 
 @app.route('/api/foo')
